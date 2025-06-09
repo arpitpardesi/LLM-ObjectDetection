b1470f60459b72cda80b1ebaf4201909f61c9fbb
Arpit Pardesi <arpit.pardesi6@gmail.com>
2025-06-10 00:09:53 +0530

    Integration with Torch for MBP

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ model = YOLO("yolov8x.pt")
 llmModel = "artifish/llama3.2-uncensored"
 
 cap = cv2.VideoCapture(0)
-last_prompt_time = 0
-cooldown = 5
+last_prompt_time = 0.0
+cooldown = 5.0
 
 # Move to MPS device
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -21,8 +21,6 @@ while True:
         break
 
     results = model(frame, verbose=False, device=device)
-    # boxes = results[0].boxes
-    # object_names = [model.names[int(cls)] for cls in boxes.cls] if boxes.id is not None else []
 
     boxes = results[0].boxes
     object_names = []
@@ -32,30 +30,15 @@ while True:
             cls_int = int(cls_id.item())
             object_names.append(model.names[cls_int])
 
-    # print("Detected objects:", set(object_names))
-
     # Draw results on the frame
     annotated_frame = results[0].plot()
 
-
-
     current_time = time.time()
-    if current_time - last_prompt_time > cooldown and object_names:
+    if (current_time - last_prompt_time) > cooldown and object_names:
         unique_objects = sorted(set(object_names))
         object_list = ", ".join(unique_objects)
 
-        # prompt = (
-        #     f"You are an intelligent assistant. "
-        #     f"The following objects are visible in a live camera feed: {object_list}. "
-        #     f"Describe what might be happening in this scene."
-        # )
-
-        # prompt = f"""You are an intelligent assistant. The following objects are visible in a live camera feed: {object_list}. Describe what might be happening in this scene."""
-        prompt = (
-            f"From the list of detected objects: {object_list}, describe in one short sentence what is most likely happening in the scene. "
-            f"Be vivid, clear, and avoid guessing or uncertainty. Do not explain — just describe the scene."
-        )
-        # print(prompt)
+        prompt = f"""From the list of detected objects: {object_list}, describe in one short sentence what is most likely happening in the scene. Be vivid, clear, and avoid guessing or uncertainty. Do not explain — just describe the scene."""
 
         response = ollama.chat(
             model=llmModel,
@@ -67,14 +50,10 @@ while True:
         )
 
         llama_response = response["message"]["content"].strip()
-        print(llama_response)
+        print("Llama: ", llama_response)
+
         # for chunk in response:
         #     print(chunk['message']['content'], end='', flush=True)
-
-        # print("\n🧠 Prompting LLaMA 3.2...\n")
-        # response = llm(prompt, max_tokens=150)
-        # text = response["choices"][0]["text"].strip()
-        # print(f"📋 LLaMA 3.2: {response}\n")
 
         last_prompt_time = current_time
 
